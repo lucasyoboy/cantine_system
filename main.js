4892d08c41c5d727e5122b31e15f0c4f1994c0b2
Lucas_yoboy <lucasheide62@gmail.com>
2024-06-29 11:18:44 -0400

    setup project

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const { app, Bro}
+const path = require('path');
+const { app, BrowserWindow} = require('electron');
+
+function createMainWindow(){
+    const mainWindow = new BrowserWindow({
+        title: "Cantina",
+        width: 500,
+        height: 600
+    });
+
+    mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+}
+
+app.whenReady().then(()=>{
+    createMainWindow();
+})
